35ef26b1116ecf7f4ccec79e10fe109072a37d87
vildeserina <53511085+vildeserina@users.noreply.github.com>
2020-05-24 01:21:06 +0200

    Updates

---
+++ precache-manifest.0cefa3b4f686834ddcab771ec5bd980c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b59c33e8ff4e52169262c5b043250a8",
+    "url": "/blog-generator/index.html"
+  },
+  {
+    "revision": "0e95b12ad31d18237cb4",
+    "url": "/blog-generator/static/css/main.48692dac.chunk.css"
+  },
+  {
+    "revision": "8999ea5d87ac1a0f1a9e",
+    "url": "/blog-generator/static/js/2.729dcd1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog-generator/static/js/2.729dcd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e95b12ad31d18237cb4",
+    "url": "/blog-generator/static/js/main.fb83ef5f.chunk.js"
+  },
+  {
+    "revision": "f5344887d1ce28a1948c",
+    "url": "/blog-generator/static/js/runtime-main.ad68af4c.js"
+  },
+  {
+    "revision": "b417614b715a944a7a5ceb39f8667879",
+    "url": "/blog-generator/static/media/CircularPro-Black.b417614b.otf"
+  },
+  {
+    "revision": "588eb6b1a02291e6c1ee19479a932769",
+    "url": "/blog-generator/static/media/CircularPro-Book.588eb6b1.woff"
+  },
+  {
+    "revision": "fcea822fb589b8a160271134a272594c",
+    "url": "/blog-generator/static/media/CircularPro-Book.fcea822f.woff2"
+  },
+  {
+    "revision": "2b1e4805c7bcc5bb8504c12374bf8bc6",
+    "url": "/blog-generator/static/media/Kult_logo.2b1e4805.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https//vildeserina.github.io/blog-generator/precache-manifest.c474678440647888c124c239085637a3.js"
+  "/blog-generator/precache-manifest.0cefa3b4f686834ddcab771ec5bd980c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//vildeserina.github.io/blog-generator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
